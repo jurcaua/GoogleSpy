889f8a1c51bf1a4f2aa966035cc8d83642e9423e
Alex <jurcaua@gmail.com>
2018-01-21 09:02:30 -0500

    gun functioanlity

---
+++ Assets/Scripts/EquipGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipGun : MonoBehaviour {
+
+    void OnTriggerEnter(Collider c) {
+        if (LayerMask.LayerToName(c.gameObject.layer).Equals("Player")) {
+
+            GetComponent<Rotater>().enabled = false;
+
+            PlayerMovement pm =  c.gameObject.GetComponent<PlayerMovement>();
+            pm.canShoot = true;
+            for (int i = 0; i < pm.transform.childCount; i++) {
+                if (pm.transform.GetChild(i).name == "GunArm") {
+                    transform.parent = pm.transform.GetChild(i);
+                    transform.localPosition = new Vector3(0.103999f, -0.224f, -0.2169999f);
+                    transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+    public Vector3 rotationMag = Vector3.one;
+    public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotationMag * speed * Time.deltaTime);
+	}
+}
